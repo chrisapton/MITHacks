609faf438145892460da2747df445500b55e04b7
Chris Apton <chrisapton@Chriss-MacBook-Pro.local>
2020-09-19 11:50:14 -0700

    some stuff

--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,14 @@ from my_app import app
 from flask import render_template, request, redirect
 import requests
 
+name="Justin Yu"
+facts = {"Birthday":"April 10th, 2000", "Favorite Color": "blue", "Favorite Hackathon": "HackMIT"}
+posts = [{"title": "This is my 1st post!", "description": "this is my first description!"}]
+
+name="Shirlyn"
+facts = {"Birthday":"February 29, 1964", "Favorite Color": "blue", "Favorite Hackathon": "HackMIT"}
+posts = [{"title": "This is my 1st post!", "description": "this is my first description!"}]
+
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html", name=name, facts=facts, posts=posts)
