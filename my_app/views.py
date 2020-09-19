ce0546663aaf5714809e6df8c5e5d14ea3f71895
Chris Apton <chrisapton@Chriss-MacBook-Pro.local>
2020-09-19 12:30:43 -0700

    3rd commit

--- my_app/views.py
+++ my_app/views.py
@@ -13,3 +13,19 @@ posts = [{"title": "This is my 1st post!", "description": "this is my first desc
 @app.route("/")
 def index():
     return render_template("index.html", name=name, facts=facts, posts=posts)
+
+@app.route("/change_name")
+def change_name():
+    global name
+    new_name = request.args.get('name')
+    name = new_name
+    return redirect("/")
+
+@app.route("/post", methods=["POST"])
+def post():
+    global posts
+    if request.method == "POST":
+        print(request)
+        post_info = request.get_json()
+        posts.append({"title": post_info["title"], "description": post_info["description"]})
+    return redirect("/")
