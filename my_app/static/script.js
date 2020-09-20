ab3553a760a733d1d3af9d365c3136e91a543b53
Jerry Li <jerryli@jerrys-mbp.attlocal.net>
2020-09-19 19:33:44 -0700

    skippidity boop bop

--- my_app/static/script.js
+++ my_app/static/script.js
@@ -23,17 +23,24 @@ $(document).ready(function() {
 
 let crowder;
 let question;
-
+let width = 300;
+let height = 219;
+var canvas;
 function setup() {
   // Code here runs only once
-  var canvas = createCanvas(300, 219);
+
   crowder = loadImage("https://cdn.glitch.com/fc616293-4b24-40ce-a4d1-55b135064e19%2Finset-change_my_mind_meme-001-300x219.jpg?v=1600559576794");
   question = "ONE PLUS ONE EQUALS 11";
-  canvas.parent('sketch-holder');
+
 }
 
 function draw() {
+  height = windowHeight/2.7;
+  width = height * 1.37 * 1.05;
+  canvas = createCanvas(width, height);
+  canvas.parent('sketch-holder');
   background(crowder);
   rotate(-PI/8);
-  text(question,50,170,100);
+  text(question,width/6,height*0.77,width/3);
+  console.log(windowWidth, windowHeight);
 }
